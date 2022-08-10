20633ad72de38f135a79afaa78981c8dfbe131aa
oNqNu <onanukintaro@gmail.com>
2022-08-10 20:21:10 +0900

    feat: タイトルとスコアのレイアウト

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,8 @@ function App() {
   return (
     <div className="App">
       <div className="contents-container">
-        <h1>Aim Trainer</h1>
+        <h1 className="title">Aim Trainer</h1>
+        <h2 className="score">Score: 00</h2>
         <div className="playarea">
           <div className="targetItem"></div>
         </div>
