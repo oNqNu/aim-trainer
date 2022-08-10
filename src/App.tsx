cc77e5e7a354f99ccc21c64e6a4fc468ddac6c2c
oNqNu <onanukintaro@gmail.com>
2022-08-10 20:04:40 +0900

    feat: prayエリア作った

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <div className="contents-container">
         <h1>Aim Trainer</h1>
+        <div className="playarea"></div>
       </div>
     </div>
   );
