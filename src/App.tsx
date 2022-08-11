8aa2472d382c0026850bb87874fdb996afc5bdae
oNqNu <onanukintaro@gmail.com>
2022-08-11 21:06:49 +0900

    feat: score の state を作った

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [score, setScore] = useState(0);
+
   return (
     <div className="App">
       <div className="contents-container">
         <h1 className="title">Aim Trainer</h1>
-        <h2 className="score">Score: 00</h2>
+        <h2 className="score">Score: {score}</h2>
         <div className="playarea">
           <div className="targetItem"></div>
         </div>
+        <button onClick={() => setScore(score + 1)}>+++</button>
       </div>
     </div>
   );
