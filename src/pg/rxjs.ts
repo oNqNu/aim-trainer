fa4496bf67d2650e8dad29fbf8c4aceee30a327c
oNqNu <onanukintaro@gmail.com>
2022-08-27 20:00:59 +0900

    pg-feat:rxjs-Subject 練習

--- src/pg/rxjs.ts
+++ src/pg/rxjs.ts
@@ -3,7 +3,7 @@ import { Subject } from 'rxjs';
 const subject = new Subject();
 
 subject.subscribe(console.log);
-subject.subscribe(console.error);
-
 subject.next(1);
+
+subject.subscribe(console.error);
 subject.next(2);
