5fb6331eff0a5b2ca30bca9a4fa5e0b990c219fe
oNqNu <onanukintaro@gmail.com>
2022-09-01 20:47:40 +0900

    rxjs-pg: subscription.add() 使ってみた

--- src/pg/rxjs.ts
+++ src/pg/rxjs.ts
@@ -1,4 +1,22 @@
-import { Observable, interval } from 'rxjs';
-const observable = interval(700);
-const subscription = observable.subscribe((x) => console.log(x));
-setTimeout(() => subscription.unsubscribe(), 1500);
+import { interval } from 'rxjs';
+
+const observable = interval(900);
+const observableChild = interval(700);
+const observableGrandChild = interval(400);
+const subscription = observable.subscribe((x) => console.log('parent: ' + x));
+const subscriptionChild = observableChild.subscribe((y) =>
+  console.log('child: ' + y)
+);
+const subscriptionGrandChild = observableGrandChild.subscribe((z) =>
+  console.log('grand child: ' + z)
+);
+subscription.add(subscriptionChild);
+subscriptionChild.add(subscriptionGrandChild);
+setTimeout(() => {
+  console.log('unsubscribe grand child');
+  subscriptionGrandChild.unsubscribe(); // 親は破棄されない
+  setTimeout(() => {
+    console.log('unsubscribe parent');
+    subscription.unsubscribe(); // 子は破棄される
+  }, 1500);
+}, 1000);
