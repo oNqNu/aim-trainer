5804dbb43227d8bcfee2a07fc805e4463bfe2c0c
oNqNu <onanukintaro@gmail.com>
2022-09-08 20:03:06 +0900

    refactor: rxjs pg の整理

--- src/pg/rxjs.ts
+++ src/pg/rxjs.ts
@@ -1,22 +1,11 @@
-import { interval } from 'rxjs';
+import { interval } from "rxjs";
 
 const observable = interval(900);
-const observableChild = interval(700);
-const observableGrandChild = interval(400);
-const subscription = observable.subscribe((x) => console.log('parent: ' + x));
-const subscriptionChild = observableChild.subscribe((y) =>
-  console.log('child: ' + y)
-);
-const subscriptionGrandChild = observableGrandChild.subscribe((z) =>
-  console.log('grand child: ' + z)
-);
-subscription.add(subscriptionChild);
-subscriptionChild.add(subscriptionGrandChild);
+const subscription = observable.subscribe((x) => console.log("parent: " + x));
 setTimeout(() => {
-  console.log('unsubscribe grand child');
-  subscriptionGrandChild.unsubscribe(); // 親は破棄されない
+  console.log("unsubscribe grand child");
   setTimeout(() => {
-    console.log('unsubscribe parent');
+    console.log("unsubscribe parent");
     subscription.unsubscribe(); // 子は破棄される
   }, 1600);
 }, 1000);
